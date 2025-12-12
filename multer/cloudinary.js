2db7cd562d1345e12cfd0b3407f235d1e77220f8
Ritin rawat <ritinrawat85@gmail.com>
2025-12-12 13:42:44 +0530

    Initial commit

--- multer/cloudinary.js
+++ multer/cloudinary.js
@@ -1,12 +1,10 @@
-
-// cloudinary.js
 const cloudinary = require("cloudinary").v2;
+require("dotenv").config();   // <-- IMPORTANT
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
-
