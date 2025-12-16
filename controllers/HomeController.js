76a1bd485a7b3fae36da9b173a3c18a4f7a45224
ritinrawat <ritinrawat378@gmail.com>
2025-12-16 12:08:41 +0530

    Remove updateBanner function from HomeController
    Removed the updateBanner function from HomeController.

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -14,30 +14,6 @@ exports.getLatestProducts = async (req, res) => {
   }
 };
 
-exports.updateBanner = async (req, res) => {
-  try {
-    const banner = await Banner.findById(req.params.id);
-    if (!banner) return res.status(404).send("Banner not found");
-
-    if (req.files?.mainBannerImage)
-      banner.mainBannerImage = req.files.mainBannerImage[0].path;
-
-    if (req.files?.cardBannerImage1)
-      banner.cardBannerImage1 = req.files.cardBannerImage1[0].path;
-
-    if (req.files?.cardBannerImage2)
-      banner.cardBannerImage2 = req.files.cardBannerImage2[0].path;
-
-    if (req.files?.cardBannerImage3)
-      banner.cardBannerImage3 = req.files.cardBannerImage3[0].path;
-
-    await banner.save();
-    res.redirect("/");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Banner update failed");
-  }
-};
 
 
 
