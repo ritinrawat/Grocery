dd6a47dfc4c136aa0fc51abbad82f180139be54d
Ritin rawat <ritinrawat85@gmail.com>
2025-12-16 12:12:05 +0530

    Initial commit

--- routes/api/HomeRoutes.js
+++ routes/api/HomeRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const homeController = require("../../controllers/HomeController");
 router.get("/getTrending", homeController.getLatestProducts );
 // router.get("/sortproducts", homeController.sortProducts );
-router.get("/getBanner", homeController.getBanner);
+// router.get("/getBanner", homeController.getBanner);
 module.exports = router;
